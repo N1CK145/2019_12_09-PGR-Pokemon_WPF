ed360fad5fec82b5305905ed61d745842ab85205
N1CK145 <nickas.gehlen@gmail.com>
2019-12-11 15:06:57 +0100

    Commit

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,17 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Runtime.Remoting.Channels;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Json;
+using System.Text;
 
 namespace PokemonWPF
 {
@@ -23,6 +25,24 @@ namespace PokemonWPF
         public MainWindow()
         {
             InitializeComponent();
+
+        }
+        private string makeRequest(string url)
+        {
+            var client = new WebClient();
+            client.Headers.Add("User-Agent", "Nothing");
+
+            client.DownloadStringCompleted += (sender, e) =>
+            {
+                var serializer = new DataContractJsonSerializer(typeof(List<Contributor>));
+                using (var ms = new MemoryStream(Encoding.Unicode.GetBytes(e.Result)))
+                {
+                    var contributors = (List<Contributor>)serializer.ReadObject(ms);
+                    contributors.ForEach(Console.WriteLine);
+                }
+            };
+
+            client.DownloadStringAsync(new Uri(url));
         }
     }
 }
